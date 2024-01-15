688512a0010471ee35063a8437e1e4d7e1e2e895
Hàns <you@example.com>
2024-01-15 17:04:38 +0100

    "buttons!.py without tkinter"

--- src/jeu/buttons.py
+++ src/jeu/buttons.py
@@ -1,32 +1,35 @@
-import tkinter as tk
-from tkinter import messagebox
+from tkinter import Tk, TkVersion, messagebox
+import tkinter
+
+from src.jeu import Jeu
+
 
 class InterfaceUtilisateur:
     def __init__(self, jeu):
         self.jeu = jeu
 
-    self.fenetre = tk.Tk()
-    self.fenetre.title("Pokémon Game")
+        self.fenetre = Tk.Tk()
+        self.fenetre.title("Pokémon Game")
 
-    self.bouton_nouvelle_partie = tk.Button(self.fenetre, text="Nouvelle Partie", command=self.commencer_partie)
-    self.bouton_nouvelle_partie.pack()
+        self.bouton_nouvelle_partie = TkVersion.Button(self.fenetre, text="Nouvelle Partie", command=self.commencer_partie)
+        self.bouton_nouvelle_partie.pack()
 
-    self.bouton_combat = tk.Button(self.fenetre, text="Combat", command=self.commencer_combat)
-    self.bouton_combat.pack()
+        self.bouton_combat = tkinter.Button(self.fenetre, text="Combat", command=self.commencer_combat)
+        self.bouton_combat.pack()
 
-def commencer_partie(self):
-    self.jeu.commencer_partie()
-    messagebox.showinfo("Information", "Nouvelle partie commencée!")
+    def commencer_partie(self):
+        self.jeu.commencer_partie()
+        messagebox.showinfo("Information", "Nouvelle partie commencée!")
 
-def commencer_combat(self):
-    adversaire = "Adversaire Pokémon"  # Remplacez par la logique de choix d'adversaire
-    self.jeu.effectuer_combat(adversaire)
-    messagebox.showinfo("Information", "Combat commencé contre {}".format(adversaire))
+    def commencer_combat(self):
+        adversaire = "Adversaire Pokémon"  # Remplacez par la logique de choix d'adversaire
+        self.jeu.effectuer_combat(adversaire)
+        messagebox.showinfo("Information", "Combat commencé contre {}".format(adversaire))
 
-def demarrer_interface(self):
-    self.fenetre.mainloop()
+    def demarrer_interface(self):
+        self.fenetre.mainloop()
     
-Exemple d'utilisation
+#Exemple d'utilisation
 jeu_pokemon = Jeu()
 interface = InterfaceUtilisateur(jeu_pokemon)
 interface.demarrer_interface()
