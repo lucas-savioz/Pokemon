a6656d1b352037a07bafaf2874960a8a906611c1
Alexandre Vidal <alexandre.vidal@laplateforme.io>
2024-01-15 11:22:12 +0100

    Revert "MàJ pokedex"
    This reverts commit 333e0f778f934a86a85466fa6d404a64d7adcbe0.

--- src/pokedex/Pokedex.py
+++ src/pokedex/Pokedex.py
@@ -1,7 +1,6 @@
 import json
-pokedex = 'pokedex.json'
 
-
-with open(pokedex, 'r') as a:
-    data = json.load(a)
-    print(data)
+with open('donnees_pokemon.json') as a:
+    b = json.load(a)
+    
+print(json.dumps(b, indent=4, ensure_ascii=False))
