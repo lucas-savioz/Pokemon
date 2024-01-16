3a1398c146cbd09ed855ba8e541cbc7fdd8af2fa
Hàns <you@example.com>
2024-01-16 14:19:57 +0100

    "upadte jeu"

---
+++ src/jeu/__init__.py
@@ -0,0 +1,34 @@
+import pygame
+
+pygame.init()
+
+# Crée une fenêtre
+screen = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption("Ma première fenêtre Pygame")
+
+# Boucle principale
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Mettez ici le reste du code pour votre jeu
+
+class Game:
+    def __init__(self):
+        self.running = True
+        self.screen = Screen()
+        self.map = Map(self.screen)
+
+    def run(self):
+        while self.running:
+            self.map.update()
+            self.screen.update()
+
+
+pygame.quit()
+
+
+
+
