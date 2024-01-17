e6b5cd463f3edc547e7e32874a039d0b12ed359b
lucas-savioz <lucas.savioz@laplateforme.io>
2024-01-17 16:56:11 +0100

    Suppression des branches : Lucas, combat, pokemon. Transfert de leurs données dans la nouvelle branche test_lucas

---
+++ assets/tilesets/tileset_general.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="tileset_general" tilewidth="16" tileheight="16" tilecount="4256" columns="8">
+ <image source="tileset_general.png" width="128" height="8512"/>
+</tileset>
---
+++ assets/tilesets/tilset_general_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="tilset_general_2" tilewidth="16" tileheight="16" tilecount="20000" columns="16">
+ <image source="tilset_general_2.png" width="256" height="20000"/>
+</tileset>
