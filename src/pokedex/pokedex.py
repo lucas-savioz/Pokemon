cfb42b9c75ab4f00e0082656a22487735417fc38
Alexandre Vidal <alexandre.vidal@laplateforme.io>
2024-01-18 14:57:00 +0100

    On reprend le Pokédex à zéro

--- src/pokedex/pokedex.py
+++ src/pokedex/pokedex.py
@@ -1,34 +1,32 @@
 import json
-from os import path
+import os
+import re
+import pygame
 
-filename = 'users.json'
-listObj = []
-
-# Check if file exists
-
-if path.isfile(filename) is False:
-    raise Exception("File not found")
-
-# Read json file
-with open(filename) as fp:
-    listObj = json.load(fp)
-    
-# Verify existing list
-print(listObj)
-print(type(listObj))
-
-listObj.append({
-    "Name": "Person 3",
-    "Age": 33,
-    "Email": "33@gmail.com"
-})
-
-# Verify updated list
-print(listObj)
-
-with open(filename, 'w') as json_file:
-    json.dump(listObj, json_file,
-              indent=4,
-              separators=(',',':'))
-    
-print('Succesfully appended to the JSON file')
+def add_pokemon(pokemon):
+    # Vérifie si le Pokémon existe
+    if not os.path.exists('pokedex.json'):
+        # Si pokedex.json n'existe pas, création d'une liste vide
+        saved_pokemon = []
+    else:
+        # Charger les Pokémon existantss depuis pokedex.json
+        try:
+            with open('pokedex.json', 'r') as file:
+                saved_pokemon = json.load(file)
+        except json.JSONDecodeError:
+            saved_pokemon = []
+            
+    # Vérifie si le Pokémon est déjà dans la liste
+    if any(entry == pokemon for entry in saved_pokemon):
+        print("Erreur : Ce Pokémon est déjà enregistré.")
+        return False
+    else:
+        # Ajoute de nouveaux Pokémon dans la liste
+        saved_pokemon.append(pokemon)
+        
+        # Ajoute le Pokémon dans le fichier pokedex.json
+        with open('pokedex.json', 'w') as file:
+            json.dump(saved_pokemon, file)
+            
+        print("Pokémon rajouté avec succès dans 'pokedex.json'")
+        return True
