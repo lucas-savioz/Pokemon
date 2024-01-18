a3a1720e509d4bcd162c044651f7a50e5f07dd1e
Alexandre Vidal <alexandre.vidal@laplateforme.io>
2024-01-18 10:16:41 +0100

    maj pokedex

--- src/pokedex/pokedex.py
+++ src/pokedex/pokedex.py
@@ -1,7 +1,34 @@
 import json
+from os import path
 
-a = open("pokedex.json", "r")
+filename = 'users.json'
+listObj = []
 
-data = json.load(a)
+# Check if file exists
 
-print(json.dumps(data, indent = 1))
+if path.isfile(filename) is False:
+    raise Exception("File not found")
+
+# Read json file
+with open(filename) as fp:
+    listObj = json.load(fp)
+    
+# Verify existing list
+print(listObj)
+print(type(listObj))
+
+listObj.append({
+    "Name": "Person 3",
+    "Age": 33,
+    "Email": "33@gmail.com"
+})
+
+# Verify updated list
+print(listObj)
+
+with open(filename, 'w') as json_file:
+    json.dump(listObj, json_file,
+              indent=4,
+              separators=(',',':'))
+    
+print('Succesfully appended to the JSON file')
